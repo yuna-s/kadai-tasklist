60a4f578f3e9c2c91a595e83a4a07d739f04dec8
yuna-s <yu00857@gmail.com>
2020-02-18 11:18:13 +0000

    add navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
+  #get 'login', to: 'sessions#new'
+  #post 'login', to: 'sessions#create'
+  #delete 'login', to: 'sessions#destroy'
+  
+  #get 'signup', to: 'users#new'
+  
   resources :tasks
 end
